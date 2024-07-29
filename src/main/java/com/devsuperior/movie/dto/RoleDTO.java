b701445518c4c0d87976482aa641266729485726
IgorTecnologia <iguigu2019@gmail.com>
2024-07-29 12:19:52 -0300

    Role DTO

---
+++ src/main/java/com/devsuperior/movie/dto/RoleDTO.java
@@ -0,0 +1,41 @@
+package com.devsuperior.movie.dto;
+
+import com.devsuperior.movie.entities.Role;
+
+public class RoleDTO {
+
+	private Long id;
+	private String authority;
+	
+	public RoleDTO() {
+	}
+	
+	public RoleDTO(Long id, String authority) {
+		this.id = id;
+		this.authority = authority;
+	}
+	
+	public RoleDTO(Role entity) {
+		id = entity.getId();
+		authority = entity.getAuthority();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getAuthority() {
+		return authority;
+	}
+
+	public void setAuthority(String authority) {
+		this.authority = authority;
+	}
+
+	
+	
+}
