15509be7c627a283927f5c7e2eaed64a55e65925
IgorTecnologia <iguigu2019@gmail.com>
2024-12-16 04:09:45 -0300

    Review Repository

---
+++ src/main/java/com/devsuperior/movie/repositories/ReviewRepository.java
@@ -0,0 +1,15 @@
+package com.devsuperior.movie.repositories;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.devsuperior.movie.entities.Review;
+
+public interface ReviewRepository extends JpaRepository<Review, UUID>{
+	
+	List<Review> findAllByTextContainingIgnoreCase(@Param("text") String text);
+
+}
