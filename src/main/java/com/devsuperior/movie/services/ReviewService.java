dadf013ea2aa32a9be34318fef23f8044106cc5d
IgorTecnologia <iguigu2019@gmail.com>
2024-12-19 01:33:02 -0300

    Review Service

--- src/main/java/com/devsuperior/movie/services/ReviewService.java
+++ src/main/java/com/devsuperior/movie/services/ReviewService.java
@@ -3,7 +3,6 @@ package com.devsuperior.movie.services;
 import com.devsuperior.movie.dto.ReviewDTO;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.UUID;
