e5ad360c4a3ea634cab204964562bb887a14eab2
IgorTecnologia <iguigu2019@gmail.com>
2024-12-19 01:27:16 -0300

    import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -23,10 +23,10 @@ SET @genre1_id(SELECT id FROM tb_genre WHERE name = 'Ação');
 SET @genre2_id(SELECT id FROM tb_genre WHERE name = 'Comédia');
 
 INSERT INTO tb_movie (id, title, sub_Title, year_Of_Release, img_Url, synopsis, genre_id) VALUES (UUID(), 'O homem de ferro', 'A batalha', 2024, 'www.img/url.com', 'Um homem com poderes incrivéis, salva o mundo enfrentando grandes batalhas', @genre1_id);
-INSERT INTO tb_movie (id, title, sub_Title, year_Of_Release, img_Url, synopsis, genre_id) VALUES (UUID(), 'O Garfild', 'O gato sapéca', 2012, 'www.img/url.com', 'Um gato para lá de esperto', @genre2_id);
+INSERT INTO tb_movie (id, title, sub_Title, year_Of_Release, img_Url, synopsis, genre_id) VALUES (UUID(), 'O Garfield', 'O gato sapéca', 2012, 'www.img/url.com', 'Um gato para lá de esperto', @genre2_id);
 
 SET @movie1_id(SELECT id FROM tb_movie WHERE title = 'O homem de ferro');
-SET @movie2_id(SELECT id FROM tb_movie WHERE title = 'O Garfild');
+SET @movie2_id(SELECT id FROM tb_movie WHERE title = 'O Garfield');
 
 INSERT INTO tb_review(id, text, movie_id, user_id) VALUES (UUID(), 'Filmaço', @movie1_id, @user1_id);
 INSERT INTO tb_review(id, text, movie_id, user_id) VALUES (UUID(), 'Muito engraçado', @movie2_id, @user2_id);
